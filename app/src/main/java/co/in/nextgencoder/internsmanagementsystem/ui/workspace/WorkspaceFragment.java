097d463ee54370faaa46f2ddc2d8dc3eecc6cdd4
NextGen-Coder <nextgenerationcoders@gmail.com>
2021-09-06 14:52:16 +0530

    Added Bottom Navigation Fragment

---
+++ app/src/main/java/co/in/nextgencoder/internsmanagementsystem/ui/workspace/WorkspaceFragment.java
@@ -0,0 +1,45 @@
+package co.in.nextgencoder.internsmanagementsystem.ui.workspace;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import co.in.nextgencoder.internsmanagementsystem.databinding.FragmentWorkspaceBinding;
+
+public class WorkspaceFragment extends Fragment {
+
+    private WorkspaceViewModel workspaceViewModel;
+    private FragmentWorkspaceBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        workspaceViewModel =
+                new ViewModelProvider(this).get(WorkspaceViewModel.class);
+
+        binding = FragmentWorkspaceBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textWorkspace;
+        workspaceViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
